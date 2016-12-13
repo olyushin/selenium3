33cd7c011357c20a6c1234706ec1d7d77f4f4a2c
Pavel Olyushin <itp2004@yandex.ru>
2016-12-14 02:14:55 +0300

    Add csharp_example to Git.

---
+++ csharp-example/csharp-example/ExampleTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            _driver.Url = "http://www.google.com/";
+            _driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            _driver.FindElement(By.Name("btnG")).Click();
+            _wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
