32ba2be6ec60e0193fc758dbf23ad88f87c4a3cc
Pavel Olyushin <itp2004@yandex.ru>
2016-12-25 17:16:50 +0300

    Homework 7: LitecartAdminMenu

---
+++ csharp-example/csharp-example/LitecartAdminMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace csharp_example
+{
+    public class LitecartAdminMenu : TestBase
+    {
+        [Test]
+        public void AdminMenu()
+        {
+            Driver.FindElement(By.Name("username")).SendKeys("admin");
+            Driver.FindElement(By.Name("password")).SendKeys("admin");
+            Driver.FindElement(By.Name("login")).Click();
+
+            var menuItemsCount = (Int64)JsExecutor.ExecuteScript("return $('[id=\"app-\"] .icon-wrapper').length");
+            for (var m = 0; m < menuItemsCount; m++)
+            {
+                ((IWebElement)JsExecutor.ExecuteScript("return $('[id=\"app-\"] .icon-wrapper')[" + m + "]")).Click();
+                Assert.True(Driver.FindElement(By.TagName("h1")).Displayed);
+
+                var submenuItemsCount = (Int64)JsExecutor.ExecuteScript("return $('[id=\"app-\"] ul li').length");
+                for (var s = 0; s < submenuItemsCount; s++)
+                {
+                    ((IWebElement)JsExecutor.ExecuteScript("return $('[id=\"app-\"] ul li')[" + s + "]")).Click();
+                    Assert.True(Driver.FindElement(By.TagName("h1")).Displayed);
+                }
+            }
+        }
+    }
+}
---
+++ csharp-example/csharp-example/TestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    public class TestBase
+    {
+        public IWebDriver Driver;
+        public WebDriverWait Wait;
+        public IJavaScriptExecutor JsExecutor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            JsExecutor = (IJavaScriptExecutor)Driver;
+
+            Driver.Url = "http://localhost/litecart/admin/";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+            Driver = null;
+        }
+    }
+}
